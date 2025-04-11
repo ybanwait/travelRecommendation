53955634de38bcf13116811619e28584ef87b30f
yadwinder <yadwinder.banwait@gmail.com>
2025-04-10 21:02:51 -0400

    Interactive work in progress

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -1,7 +1,18 @@
 const apiUrl="travel_recommendation_api.json"
 
-fetch(apiUrl)
-    .then(response => response.json())
-    .then(data=>{
-        console.log(data)
+function showRecommendation(event){
+    event.preventDefault()
+    var keyword = document.getElementById("dest_search").value;
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
+
+        }
+            
+    })
+    .catch(error => {
+        console.error("Error fetching data:", error)
     })          
+}    
+
+document.getElementById("search_form").addEventListener('submit', showRecommendation)
